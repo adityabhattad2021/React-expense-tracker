bef067e1404109dadeecf3fb24b20fda69dd8011
Aditya Bhattad <adityabhattad18@gmail.com>
2022-09-24 22:21:33 +0530

    React Components example

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,22 @@
+import Card from "../UI/Card";
+import ExpenseItem from "./ExpenseItem";
+import "./Expenses.css";
+
+function Expenses(props) {
+	return (
+		<Card className="expenses">
+			{props.expenses.map((expense) => {
+				return (
+					<ExpenseItem
+						key={expense.id}
+						title={expense.title}
+						amount={expense.amount}
+						date={expense.date}
+					/>
+				);
+			})}
+		</Card>
+	);
+}
+
+export default Expenses;
